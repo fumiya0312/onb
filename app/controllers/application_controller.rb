0b77a475ea5755d4c74db06ac1a9095b4453d069
fumiya0312 <good.job0312@gmail.com>
2019-02-20 11:52:10 +0900

    cancancan導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  authorize_resource :class => false
+  rescue_from CanCan::AccessDenied do |exception|
+    # root_urlにかっ飛ばす。
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
-  def show
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :role)
   end
 end
